0d3c78c9919dec177f7357315cfce81260cc049f
Miguel <mbernedo5@gmail.com>
2017-11-02 23:14:53 -0500

    1

---
+++ build/web/javascripts/principal.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var id = localStorage.getItem("usuario");
+
+$(document).ready(function () {
+    var jqxhr = $.get(URL + "getBooks?idUser=" + id).done(function (data) {
+        if (data.cod === 1) {
+            var arr = data.data;
+            arr.forEach(function (item, index) {
+                $("#books").append("<tr>" +
+                        "<th>" + item.idBook + "</th>" +
+                        "<td>" + item.titulo + "</td>" +
+                        "<td>" + item.genero + "</td>" +
+                        "<td><button class='btn btn-success'>Si</button><button class='btn btn-danger'>No</button></td>" +
+                        "</tr>");
+            });
+        }
+    }).fail(function () {
+        console.log("nel");
+    });
+});
